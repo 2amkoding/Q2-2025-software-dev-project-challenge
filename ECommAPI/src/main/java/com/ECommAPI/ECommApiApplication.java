233404cc8c75857910c4256c7e22c8ccc800a7a0
2amkoding <2amkoding@gmail.com>
2025-05-01 14:31:28 -0500

    [backend] Setup - SpringBoot

---
+++ ECommAPI/src/main/java/com/ECommAPI/ECommApiApplication.java
@@ -0,0 +1,13 @@
+package com.ECommAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommApiApplication.class, args);
+	}
+
+}
---
+++ ECommAPI/src/test/java/com/ECommAPI/ECommApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECommAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
